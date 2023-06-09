d954af15c599359905d223e4f1450e11117e9e1a
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-06-09 02:34:11 +0100

    Done with Twitter Clone API using Django Restframework

--- backend/settings.py
+++ backend/settings.py
@@ -172,7 +172,8 @@ CORS_ORIGIN_WHITELIST = (
     'https://twitter-frontend-clone.vercel.app',
 )
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
