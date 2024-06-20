4fcc5500b2ab6e7ef9d598e32be397a3333f187c
Brilliant Makanju <106150499+brilliantmakanju@users.noreply.github.com>
2024-06-20 10:03:38 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 python3.9 -m virtualenv development
 source development/bin/activate
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
